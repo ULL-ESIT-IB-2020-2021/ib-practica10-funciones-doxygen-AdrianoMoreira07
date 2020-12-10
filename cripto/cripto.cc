56aa1d36b246d835735b793e7f3dfe1763b5f9eb
adriano <adriano.moreira.07@ull.edu.es>
2020-12-10 19:16:03 +0000

    Creacion del directorio del proyecto cripto

---
+++ cripto/cripto.cc
@@ -0,0 +1,19 @@
+/**
+* Universidad de La Laguna; Escuela Superior de Ingeniería y Tecnología
+* Grado en Ingeniería Informática (1º)
+* Asignatura: Informática Básica
+* 
+* @author Adriano dos Santos Moreira <alu0101436784@ull.edu.es> 
+* @brief //DESC
+* 
+* 
+*/
+
+
+
+int main () {
+
+
+
+  return 0;
+}
---
+++ cripto/funciones_cripto.h
@@ -0,0 +1,11 @@
+/**
+* Universidad de La Laguna; Escuela Superior de Ingeniería y Tecnología
+* Grado en Ingeniería Informática (1º)
+* Asignatura: Informática Básica
+* 
+* @author Adriano dos Santos Moreira <alu0101436784@ull.edu.es> 
+* @brief header
+* 
+* 
+*/
+
